34fd5d3fabdd97851e2781aadef4a3ac5406ec5c
Natoltora123 <nhasimbolatoavina@gmail.com>
2026-06-05 11:28:39 +0300

    feat: add admin speakers CRUD pages

---
+++ src/resources/speakers/SpeakerCreate.tsx
@@ -0,0 +1,26 @@
+import { Create, SimpleForm, TextInput, required } from "react-admin";
+
+export function SpeakerCreate() {
+  return (
+    <Create title="Create speaker">
+      <SimpleForm>
+        <TextInput source="name" label="Name" validate={required()} fullWidth />
+        <TextInput source="role" label="Role" validate={required()} fullWidth />
+        <TextInput source="specialty" label="Specialty" fullWidth />
+        <TextInput source="company" label="Company" fullWidth />
+
+        <TextInput source="bio" label="Bio" multiline rows={5} fullWidth />
+
+        <TextInput source="photo" label="Photo URL" fullWidth />
+        <TextInput source="initials" label="Initials" fullWidth />
+
+        <TextInput source="linkedin" label="LinkedIn" fullWidth />
+        <TextInput source="twitter" label="Twitter" fullWidth />
+        <TextInput source="website" label="Website" fullWidth />
+
+        <TextInput source="day" label="Day" fullWidth />
+        <TextInput source="sessionType" label="Session type" fullWidth />
+      </SimpleForm>
+    </Create>
+  );
+}
