7c0f9f6aee1eb1494e4b4296cf5c260e446f5d9f
Dylan RANDRIANARIVELO <hei.ambinintsoa@gmail.com>
2026-06-28 13:46:52 +0300

    Revert "Feat/session speaker assignment"

--- src/resources/rooms/RoomEdit.tsx
+++
@@ -1,9 +0,0 @@
-import { Edit, SimpleForm, TextInput, required } from "react-admin";
-
-export const RoomEdit = () => (
-    <Edit>
-        <SimpleForm>
-            <TextInput source="name" label="Name" validate={required()} fullWidth />
-        </SimpleForm>
-    </Edit>
-);
